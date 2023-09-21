601aa4191fea04a60344264abd121f9c7f4d552b
indira-faizullina <indiramus@list.ru>
2023-09-21 16:08:44 +1100

    change demo-expenses file

--- src/demo-expenses.js
+++ src/demo-expenses.js
@@ -1,10 +1,40 @@
 const demoExpenses = [
-    {title: 'Покупки в Магнит', price: 1320, category: {categoryName: 'Продукты', img: 'products.png'}, id: 1},
-    {title: 'Лампочки', price: 410, category: {categoryName: 'Дом', img: '}, id: 2},
-    {title: 'Рубашка', price: 1999, category: {categoryName: 'Одежда', img: }, id: 3},
-    {title: 'Витамин D', price: 850, category: {categoryName: 'Здоровье', img: }, id: 4},
-    {title: 'Бизнес-ланч', price: 400, category: {categoryName: 'Кафе и рестораны', img: }, id: 5},
-    {title: 'Бензин', price: 1500, category: {categoryName: 'Транспорт', img: }, id: 6},
-];
+  {
+    title: 'Покупки в Магнит',
+    price: 1320,
+    category: { categoryName: 'Продукты', img: 'products.png' },
+    id: 1,
+  },
+  {
+    title: 'Лампочки',
+    price: 410,
+    category: { categoryName: 'Дом', img: 'home.png' },
+    id: 2,
+  },
+  {
+    title: 'Рубашка',
+    price: 1999,
+    category: { categoryName: 'Одежда', img: 'clothes' },
+    id: 3,
+  },
+  {
+    title: 'Витамин D',
+    price: 850,
+    category: { categoryName: 'Здоровье', img: 'health.png' },
+    id: 4,
+  },
+  {
+    title: 'Бизнес-ланч',
+    price: 400,
+    category: { categoryName: 'Кафе и рестораны', img: 'cafe.png' },
+    id: 5,
+  },
+  {
+    title: 'Бензин',
+    price: 1500,
+    category: { categoryName: 'Транспорт', img: 'transport.png' },
+    id: 6,
+  },
+]
 
-export default demoPizzas;
+export default demoPizzas
