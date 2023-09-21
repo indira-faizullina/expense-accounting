da8b10a465adcc4170280706bbd8e3a7720c21d1
indira-faizullina <indiramus@list.ru>
2023-09-21 11:03:03 +1100

    first commit

---
+++ src/demo-expenses.js
@@ -0,0 +1,10 @@
+const demoExpenses = [
+    {title: 'Покупки в Магнит', price: 1320, category: {categoryName: 'Продукты', img: 'products.png'}, id: 1},
+    {title: 'Лампочки', price: 410, category: {categoryName: 'Дом', img: '}, id: 2},
+    {title: 'Рубашка', price: 1999, category: {categoryName: 'Одежда', img: }, id: 3},
+    {title: 'Витамин D', price: 850, category: {categoryName: 'Здоровье', img: }, id: 4},
+    {title: 'Бизнес-ланч', price: 400, category: {categoryName: 'Кафе и рестораны', img: }, id: 5},
+    {title: 'Бензин', price: 1500, category: {categoryName: 'Транспорт', img: }, id: 6},
+];
+
+export default demoPizzas;
